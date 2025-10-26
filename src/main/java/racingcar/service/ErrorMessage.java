04089e7bcdefde3fb19db0e3463e22ca8b56c9c6
choyeongju <jjudy1301@naver.com>
2025-10-26 22:58:19 +0900

    refactor: 에러 메세지 상수화 클래스

---
+++ src/main/java/racingcar/service/ErrorMessage.java
@@ -0,0 +1,10 @@
+package racingcar.service;
+
+public final class ErrorMessage {
+    private ErrorMessage() {}
+
+    public static final String EMPTY_NAMES = "이름은 비어 있을 수 없습니다.";
+    public static final String INVALID_NAME_LENGTH = "각 이름은 1~5자여야 합니다.";
+    public static final String TRYCOUNT_POSITIVE = "시도 횟수는 자연수여야 합니다.";
+    public static final String CAR_NOT_FOUND = "해당 이름의 자동차를 찾을 수 없습니다.";
+}
