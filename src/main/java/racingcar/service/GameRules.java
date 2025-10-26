ec45e44bc1fe636034c86a262cafd07e12d207b3
choyeongju <jjudy1301@naver.com>
2025-10-26 22:58:10 +0900

    refactor: 상수 정의 전용 클래스

---
+++ src/main/java/racingcar/service/GameRules.java
@@ -0,0 +1,11 @@
+package racingcar.service;
+
+public final class GameRules {
+    private GameRules() {}
+
+    public static final int RANDOM_MIN = 0;
+    public static final int RANDOM_MAX = 9;
+    public static final int MOVE_THRESHOLD = 4;
+
+    public static final int MAX_NAME_LENGTH = 5;
+}
